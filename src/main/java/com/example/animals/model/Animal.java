d336ade2c1f70510515500237a6ecffcb678d9a6
Joseph <nguld@itsj-group.com>
2020-02-11 21:45:14 +0700

    initial module A.2

--- src/main/java/com/example/animals/model/Animal.java
+++ src/main/java/com/example/animals/model/Animal.java
@@ -1,12 +1,23 @@
 package com.example.animals.model;
 
+import com.example.animals.service.CanFly;
+import com.example.animals.service.CanSing;
 import com.example.animals.service.CanWalk;
 
-public class Animal implements CanWalk {
+public class Animal implements CanWalk, CanFly, CanSing {
 
   @Override
   public void walk() {
     System.out.println("I am walking");
   }
 
+  @Override
+  public void fly() {
+    System.out.println("I am flying");
+  }
+
+  @Override
+  public void sing() {
+    System.out.println("I am singing");
+  }
 }
--- src/main/java/com/example/animals/model/Bird.java
+++ src/main/java/com/example/animals/model/Bird.java
@@ -2,12 +2,4 @@ package com.example.animals.model;
 
 public class Bird extends Animal {
 
-  public void fly() {
-    System.out.println("I am flying");
-  }
-
-  public void sing() {
-    System.out.println("I am singing");
-  }
-
 }
